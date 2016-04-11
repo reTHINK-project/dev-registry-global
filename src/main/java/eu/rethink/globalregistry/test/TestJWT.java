5886ec7067ca02f2d62ab4615ed6aa2ac17e8ab1
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-04-11 11:41:32 +0200

    JWT test

--- src/main/java/eu/rethink/globalregistry/test/TestJWT.java
+++ src/main/java/eu/rethink/globalregistry/test/TestJWT.java
@@ -8,6 +8,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.Base64UrlCodec;
 
 import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 import eu.rethink.globalregistry.model.GUID;
 import eu.rethink.globalregistry.util.ECDSAKeyPairManager;
 
@@ -19,11 +22,29 @@ public class TestJWT
 	{
 		try
 		{
+			String privateKeyString = "-----BEGIN PRIVATE KEY-----MEUCAQAwEAYHKoZIzj0CAQYFK4EEAAoELjAsAgEBBCDZRNY2W/5YH181gGXRnuzID3Y8oOpo+76yShhH3sdOiKEFAwMAAAA=-----END PRIVATE KEY-----";
+			String publicKeyString = "-----BEGIN PUBLIC KEY-----MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEkIf9IjfhT6whPtZJayz8huQAR5C3A0HMFT5K0yR9JMdfZHDDSg/PGSpFPng0GzKzQmbQIJkjUvG8HERSnePC2A==-----END PUBLIC KEY-----";
+			String datasetString = "{\"userIDs\":[],\"publicKey\":\"-----BEGIN PUBLIC KEY-----MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEkIf9IjfhT6whPtZJayz8huQAR5C3A0HMFT5K0yR9JMdfZHDDSg/PGSpFPng0GzKzQmbQIJkjUvG8HERSnePC2A==-----END PUBLIC KEY-----\",\"salt\":\"on+YI40sHALyoTHYlNKUEPCRXZ+TC/+AOJ5f4LVeJDw=\",\"guid\":\"fmb98kDf5LNFyw4YTthvyWdCThxfXKPW4P7cXqIHdVc\",\"lastUpdate\":\"2016-04-08T17:23:41.948Z\",\"timeout\":\"2026-04-08T17:23:41.948Z\",\"active\":1,\"revoked\":0}";
+			
+			//////////////////////////////////////////////////
+			
+			System.out.print("creating keys from String... ");
+			
+			PrivateKey privateKey = ECDSAKeyPairManager.decodePrivateKey(privateKeyString);
+			PublicKey publicKey = ECDSAKeyPairManager.decodePublicKey(publicKeyString);
+			
+			//String publicKeyString = ECDSAKeyPairManager.encodePublicKey(keypair.getPublic());
+			//String privateKeyString = ECDSAKeyPairManager.encodePrivateKey(keypair.getPrivate());
+			
+			System.out.println("ok");
+			System.out.println("\n  [ PrivateKey: " + privateKeyString + " ] \n expected: " + ECDSAKeyPairManager.encodePrivateKey(privateKey));
+			System.out.println("  [ PublicKey: " + publicKeyString + " ] \n expected: " + ECDSAKeyPairManager.encodePublicKey(publicKey) + "\n");
+			
 			//////////////////////////////////////////////////
 			
 			System.out.print("creating JSONObject from String... ");
 			
-			JSONObject json = new JSONObject("{\"userIDs\":[],\"publicKey\":\"-----BEGIN PUBLIC KEY-----MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEkIf9IjfhT6whPtZJayz8huQAR5C3A0HMFT5K0yR9JMdfZHDDSg/PGSpFPng0GzKzQmbQIJkjUvG8HERSnePC2A==-----END PUBLIC KEY-----\",\"salt\":\"on+YI40sHALyoTHYlNKUEPCRXZ+TC/+AOJ5f4LVeJDw=\",\"guid\":\"fmb98kDf5LNFyw4YTthvyWdCThxfXKPW4P7cXqIHdVc\",\"lastUpdate\":\"2016-04-08T17:23:41.948Z\",\"timeout\":\"2026-04-08T17:23:41.948Z\",\"active\":1,\"revoked\":0}");
+			JSONObject json = new JSONObject(datasetString);
 			System.out.println("ok");
 			System.out.println("\n  [ JSON: " + json.toString() + " ]\n");
 			
@@ -35,6 +56,31 @@ public class TestJWT
 			
 			System.out.println("ok");
 			System.out.println("\n  [ encoded: " + encodedClaim + " ]\n");
+			
+			//////////////////////////////////////////////////
+						
+			System.out.print("creating JWT... ");
+			String jwt = Jwts.builder().claim("data", encodedClaim).signWith(SignatureAlgorithm.ES256, privateKey).compact();
+			
+			System.out.println("ok");
+			System.out.println("\n  [ jwt: " + jwt + " ]\n");
+			
+			//////////////////////////////////////////////////
+			
+			System.out.print("Writing new JWT to GlobalRegistry [Node1] (PUT new dataset)... ");
+			
+			GlobalRegistryAPI.putData(gRegNode1, json.getString("guid"), jwt);
+			
+			System.out.println("ok");
+			
+			//////////////////////////////////////////////////
+			
+			System.out.print("Fetching JWT from GlobalRegistry [Node1] (GET existing dataset)... ");
+			
+			jwt = GlobalRegistryAPI.getData(gRegNode1, json.getString("guid"));
+			
+			System.out.println("ok");
+			System.out.println("\n  [ jwt: " + jwt + " ]\n");
 			
 			//////////////////////////////////////////////////
 		}
