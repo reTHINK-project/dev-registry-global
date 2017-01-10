4e861a95be3bd6eea5c4cbd5fe86d60153bfa9d2
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2017-01-10 15:47:04 +0100

    reworked config handling at daemon startup code cleanup

--- src/main/java/com/MVC/GlobalRegistryServer.java
+++ src/main/java/com/MVC/GlobalRegistryServer.java
@@ -2,7 +2,7 @@ package com.MVC;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.MVC.configuration.Configuration;
+import com.MVC.configuration.Config;
 import com.MVC.dht.DHTManager;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -20,124 +20,152 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 
 /**
- * Created by Half-Blood on 1/4/2017.
+ * Main class for GlobalRegistry daemon
+ * 
+ * @date 10.01.2017
+ * @version 1
+ * @author Sebastian Göndör, Parth Singh
  */
 @SpringBootApplication
-public class GlobalRegistryServer implements Daemon {
-     private static Logger LOGGER;
-
-    public static void main(String[] args) {
-        // init SONIC, configure
-        Options options = new Options();
-
-        OptionBuilder.withLongOpt("help");
-        OptionBuilder.withDescription("displays help on cli parameters");
-        Option helpOption = OptionBuilder.create("h");
-
-        OptionBuilder.withLongOpt("cfg");
-        OptionBuilder
-                .withDescription("required: give the path to the folder that contains greg.config");
-        //OptionBuilder.isRequired();
-        OptionBuilder.hasArg();
-        Option cfgOption = OptionBuilder.create("c");
-
-        options.addOption(helpOption);
-        options.addOption(cfgOption);
-
-        // parse comman line parameters
-        CommandLineParser parser = new BasicParser();
-
-        try{
-            CommandLine cmd = parser.parse(options, args);
-            if (cmd.hasOption("h"))
-            {
-                HelpFormatter formater = new HelpFormatter();
-                formater.printHelp("gRegs help", options);
-                System.exit(0);
-            }
-
-            String configFile;
-
-            //configFile = Configuration.getInstance().getConfigFilename();
-
-            if(cmd.hasOption("c"))
-            {
-                System.out.println(cmd.getOptionValue("c"));
-                configFile = cmd.getOptionValue("c");
-            }
-            else
-            {
-                configFile = Configuration.getInstance().getConfigFilename();
-            }
-
-            System.out.println("Initializing GlobalRegistry - loading greg.config");
-            String pathToConfig = configFile;
-            //String fullPathToConfig = pathToConfig + File.separator + "greg.config";
-            String fullPathToConfig = pathToConfig;
-            System.out.println("Trying to load config file: " + fullPathToConfig);
-
-            if(!Configuration.propFileExists(fullPathToConfig))
-            {
-                System.out.println("No configuration file found. Please create valid gsls.config and provide path to it via command line option -c.");
-                System.exit(0);
-            }
-
-            Configuration.getInstance().loadConfigurationFile();
-
-            // setup logging
-            System.setProperty("loginfofile", Configuration.getInstance().getLogPath() + "log-info.log");
-            System.setProperty("logdebugfile", Configuration.getInstance().getLogPath() + "log-debug.log");
-             LOGGER = LoggerFactory.getLogger(GlobalRegistryServer.class);
-
-            LOGGER.info(Configuration.getInstance().getProductName() + " "
-                + Configuration.getInstance().getVersionName() + " "
-                + Configuration.getInstance().getVersionCode());
-            LOGGER.info("Build #" + Configuration.getInstance().getVersionNumber() + " ("
-              + Configuration.getInstance().getVersionDate() + ")\n");
-        } catch (ParseException e) {
-            System.out.println("Wrong parameter. Error: " + e.getMessage());
-        }
-        LOGGER.info("initializing DHT... ");
-
-        try{
-            DHTManager.getInstance().initDHT();
-            LOGGER.info("DHT initialized successfully");
-
-            // init GlobalRegistry server
-            LOGGER.info("initializing Global Registry server... ");
-
-            //JettyServer.start();
-            System.getProperties().put("server.port", 5002);
-            SpringApplication.run(GlobalRegistryServer.class, args);
-        }
-        catch (Exception e)
-        {
-            LOGGER.info("failed!");
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void init(DaemonContext daemonContext) throws DaemonInitException, Exception {
-        System.out.println("deamon: init()");
-        String arguments[] = daemonContext.getArguments();
-        System.out.println(arguments);
-        GlobalRegistryServer.main(arguments);
-    }
-
-    @Override
-    public void start() throws Exception {
-        System.out.println("deamon: start()");
-    }
-
-    @Override
-    public void stop() throws Exception {
-        System.out.println("deamon: exception()");
-    }
-
-    @Override
-    public void destroy() {
-        System.out.println("deamon: destroy()");
-    }
+public class GlobalRegistryServer implements Daemon
+{
+	private static Logger LOGGER;
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Initializing GlobalRegistry");
+		
+		Config config = Config.getInstance();
+		
+		Options options = new Options();
+		
+		OptionBuilder.withLongOpt("help");
+		OptionBuilder.withDescription("displays help on cli parameters");
+		Option helpOption = OptionBuilder.create("h");
+		
+		OptionBuilder.withLongOpt("port_rest");
+		OptionBuilder.withDescription("sets the port for the REST interface [" + config.getPortREST() + "]");
+		OptionBuilder.hasArg();
+		Option portRESTOption = OptionBuilder.create("p");
+		
+		OptionBuilder.withLongOpt("network_interface");
+		OptionBuilder.withDescription("sets the network interface [" + config.getNetworkInterface() + "]");
+		OptionBuilder.hasArg();
+		Option networkInterfaceOption = OptionBuilder.create("n");
+		
+		OptionBuilder.withLongOpt("log_path");
+		OptionBuilder.withDescription("sets the directory for the log files [" + config.getLogPath() + "]");
+		OptionBuilder.hasArg();
+		Option logPathOption = OptionBuilder.create("l");
+		
+		OptionBuilder.withLongOpt("connect_node");
+		OptionBuilder.withDescription("sets the GReg node to connect to [" + config.getConnectNode() + "]");
+		OptionBuilder.hasArg();
+		Option connectNodeOption = OptionBuilder.create("c");
+		
+		options.addOption(helpOption);
+		options.addOption(portRESTOption);
+		options.addOption(networkInterfaceOption);
+		options.addOption(logPathOption);
+		options.addOption(connectNodeOption);
+		
+		// parse comman line parameters
+		CommandLineParser parser = new BasicParser();
+		
+		try
+		{
+			CommandLine cmd = parser.parse(options, args);
+			if(cmd.hasOption("h"))
+			{
+				HelpFormatter formater = new HelpFormatter();
+				formater.printHelp("GReg help", options);
+				System.exit(0);
+			}
+			
+			if(cmd.hasOption("p"))
+			{
+				config.setPortREST(Integer.parseInt(cmd.getOptionValue("p"))); // TODO check for valid values
+			}
+			if(cmd.hasOption("n"))
+			{
+				config.setNetworkInterface(cmd.getOptionValue("n")); // TODO check for valid values
+			}
+			if(cmd.hasOption("l"))
+			{
+				config.setLogPath(cmd.getOptionValue("l")); // TODO check for valid values
+			}
+			if(cmd.hasOption("c"))
+			{
+				config.setConnectNode(cmd.getOptionValue("c")); // TODO check for valid values
+			}
+			
+			System.out.println("\n-----Configuration: ");
+			System.out.println("connectNode: " + config.getConnectNode());
+			System.out.println("portREST: " + config.getPortREST());
+			System.out.println("networkInterface: " + config.getNetworkInterface());
+			System.out.println("logPath: " + config.getLogPath() + "\n-----");
+			
+			// setup logging
+			System.setProperty("loginfofile", Config.getInstance().getLogPath() + "log-info.log");
+			System.setProperty("logdebugfile", Config.getInstance().getLogPath() + "log-debug.log");
+			LOGGER = LoggerFactory.getLogger(GlobalRegistryServer.class);
+			
+			LOGGER.info(Config.getInstance().getProductName() + " "
+					+ Config.getInstance().getVersionName() + " "
+					+ Config.getInstance().getVersionCode());
+			
+			LOGGER.info("Build #" + Config.getInstance().getVersionNumber() + " ("
+					+ Config.getInstance().getVersionDate() + ")\n");
+		}
+		catch (ParseException e)
+		{
+			System.out.println("Wrong parameter. Error: " + e.getMessage());
+		}
+		LOGGER.info("initializing DHT... ");
+		
+		try
+		{
+			DHTManager.getInstance().initDHT();
+			LOGGER.info("DHT initialized successfully");
+		
+			// init GlobalRegistry server
+			LOGGER.info("initializing Global Registry server... ");
+			
+			//JettyServer.start();
+			System.getProperties().put("server.port", 5002);
+			SpringApplication.run(GlobalRegistryServer.class, args);
+		}
+		catch (Exception e)
+		{
+			LOGGER.info("failed!");
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void init(DaemonContext daemonContext) throws DaemonInitException, Exception
+	{
+		System.out.println("deamon: init()");
+		String arguments[] = daemonContext.getArguments();
+		System.out.println(arguments);
+		GlobalRegistryServer.main(arguments);
+	}
+	
+	@Override
+	public void start() throws Exception
+	{
+		System.out.println("deamon: start()");
+	}
+	
+	@Override
+	public void stop() throws Exception
+	{
+		System.out.println("deamon: exception()");
+	}
+	
+	@Override
+	public void destroy()
+	{
+		System.out.println("deamon: destroy()");
+	}
 }
-
