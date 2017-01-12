6628f75095f860b1bdc94f8d04bca3c498f5d3a6
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2017-01-12 11:01:49 +0100

    updated Dataset.java for legacyID support

--- src/main/java/eu/rethink/globalregistry/model/DatasetIntegrityException.java
+++ src/main/java/eu/rethink/globalregistry/model/DatasetIntegrityException.java
@@ -1,24 +1,28 @@
 package eu.rethink.globalregistry.model;
 
 /**
- * Created by Half-Blood on 1/4/2017.
+ * Exception class for handling integrity check failures of the dataset
+ * 
+ * @date 12.01.2017
+ * @version 1
+ * @author Sebastian Göndör
  */
-public class DatasetIntegrityException extends Exception{
-
-    private static final long serialVersionUID = 8787254843105440320L;
-
-    public DatasetIntegrityException(String message)
-    {
-        super(message);
-    }
-
-    public DatasetIntegrityException(String message, Throwable cause)
-    {
-        super(message, cause);
-    }
-
-    public DatasetIntegrityException(Throwable cause)
-    {
-        super(cause);
-    }
+public class DatasetIntegrityException extends Exception
+{
+	private static final long serialVersionUID = 8787254843105440320L;
+	
+	public DatasetIntegrityException(String message)
+	{
+		super(message);
+	}
+	
+	public DatasetIntegrityException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+	
+	public DatasetIntegrityException(Throwable cause)
+	{
+		super(cause);
+	}
 }
