fb5663fe0f399d7b36be79b6da7d9f26f1989848
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2017-04-03 15:54:25 +0200

    added (temporarily disables) reconnect interval parameter

--- src/main/java/eu/rethink/globalregistry/configuration/Config.java
+++ src/main/java/eu/rethink/globalregistry/configuration/Config.java
@@ -12,35 +12,37 @@ import java.net.UnknownHostException;
  */
 public class Config
 {
-	private static Config _singleton = null;
-
-	private static final String		versionName				= "0.3.3";
-	private static final int		versionNumber			= 1578;
-	private static final String		versionCode				= "springified";
-	private static final String		versionDate				= "2017-03-29";
-	private static final String		productName				= "reTHINK Global Registry";
-	private static final String		productNameShort		= "gReg";
-
-	private static final int		portDHT					= 5001;
-	private static final int		versionDatasetSchema	= 2;
-	private static final int		versionRESTAPI			= 1;
-	private static final int		versionDHTAPI			= 1;
-
-	private static final String		networkInterfaceDefault	= "eth0";
-	private static final String		logPathDefault			= "logs";
-	private static final String		connectNodeDefault		= "130.149.22.220";
-	private static final int		portRESTDefault			= 5002;
-
-	private String networkInterface;
-	private String logPath;
-	private String connectNode;
-	private int portREST;
-
+	private static Config		_singleton					= null;
+	
+	private static final String	versionName					= "0.3.4";
+	private static final int	versionNumber				= 1593;
+	private static final String	versionCode					= "springified";
+	private static final String	versionDate					= "2017-04-03";
+	private static final String	productName					= "reTHINK Global Registry";
+	private static final String	productNameShort			= "gReg";
+	
+	private static final int	portDHT						= 5001;
+	private static final int	versionDatasetSchema		= 2;
+	private static final int	versionRESTAPI				= 1;
+	private static final int	versionDHTAPI				= 1;
+	
+	//private static final int	reconnectIntervalDefault	= 12;
+	private static final String	networkInterfaceDefault		= "eth0";
+	private static final String	logPathDefault				= "logs";
+	private static final String	connectNodeDefault			= "130.149.22.220";
+	private static final int	portRESTDefault				= 5002;
+	
+	//private int					reconnectInterval;
+	private String				networkInterface;
+	private String				logPath;
+	private String				connectNode;
+	private int					portREST;
+	
 	private Config()
 	{
 		setDefaultValues();
 	}
-
+	
 	public static Config getInstance()
 	{
 		if(_singleton == null)
@@ -49,36 +51,53 @@ public class Config
 		}
 		return _singleton;
 	}
-
+	
 	private void setDefaultValues()
 	{
+		//this.reconnectInterval = reconnectIntervalDefault;
 		this.networkInterface = networkInterfaceDefault;
 		this.connectNode = connectNodeDefault;
 		this.logPath = logPathDefault; // TODO check if this is working
 		this.portREST = portRESTDefault;
 	}
-
-	public String getNetworkInterface() {
+	
+//	public int getReconnectInterval()
+//	{
+//		return reconnectInterval;
+//	}
+//	
+//	public void setReconnectInterval(int reconnectInterval)
+//	{
+//		this.reconnectInterval = reconnectInterval;
+//	}
+	
+	public String getNetworkInterface()
+	{
 		return networkInterface;
 	}
-
-	public void setNetworkInterface(String networkInterface) {
+	
+	public void setNetworkInterface(String networkInterface)
+	{
 		this.networkInterface = networkInterface;
 	}
-
-	public String getLogPath() {
+	
+	public String getLogPath()
+	{
 		return logPath;
 	}
-
-	public void setLogPath(String logPath) {
+	
+	public void setLogPath(String logPath)
+	{
 		this.logPath = logPath;
 	}
-
-	public String getConnectNode() {
+	
+	public String getConnectNode()
+	{
 		return connectNode;
 	}
-
-	public void setConnectNode(String connectNode) {
+	
+	public void setConnectNode(String connectNode)
+	{
 		this.connectNode = connectNode;
 	}
 	
@@ -95,27 +114,30 @@ public class Config
 		{
 			address = InetAddress.getByName(connectNodeURL);
 		}
-		catch (UnknownHostException e)
+		catch(UnknownHostException e)
 		{
 			this.connectNode = connectNodeDefault;
 			return;
 		}
 		
-		this.connectNode = address.getHostAddress(); 
+		this.connectNode = address.getHostAddress();
 	}
-
-	public int getPortREST() {
+	
+	public int getPortREST()
+	{
 		return portREST;
 	}
-
-	public void setPortREST(int portREST) {
+	
+	public void setPortREST(int portREST)
+	{
 		this.portREST = portREST;
 	}
-
-	public int getPortDHT() {
+	
+	public int getPortDHT()
+	{
 		return portDHT;
 	}
-
+	
 	/**
 	 * retrieves the product name as a String
 	 *
@@ -125,7 +147,7 @@ public class Config
 	{
 		return productName;
 	}
-
+	
 	/**
 	 * retrieves the product name short as a String
 	 *
@@ -135,7 +157,7 @@ public class Config
 	{
 		return productNameShort;
 	}
-
+	
 	/**
 	 * retrieves the version number as a String, e.g. "0.1.2"
 	 *
@@ -145,7 +167,7 @@ public class Config
 	{
 		return versionName;
 	}
-
+	
 	/**
 	 * retrieves the date of the build, e.g. 2014-08-22
 	 *
@@ -155,27 +177,27 @@ public class Config
 	{
 		return versionDate;
 	}
-
+	
 	public int getVersionNumber()
 	{
 		return versionNumber;
 	}
-
+	
 	public String getVersionCode()
 	{
 		return versionCode;
 	}
-
+	
 	public int getVersaionDatasetSchema()
 	{
 		return versionDatasetSchema;
 	}
-
+	
 	public int getVersionRESTAPI()
 	{
 		return versionRESTAPI;
 	}
-
+	
 	public int getVersionDHTAPI()
 	{
 		return versionDHTAPI;
